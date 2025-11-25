27dc52958974a6340ae237ce95b56e082826278b
David <syllad119@gmail.com>
2025-11-25 13:29:08 +0100

    firstcommit

---
+++ test.js
@@ -0,0 +1,48 @@
+/*Ici un commentaire comme en  css 
+
+*/
+
+// un commentaire sur une ligne
+
+
+// console.log('Hello  world !');
+
+// console.log("hello world");
+// Variable
+// let nom = "Jean";
+
+// Array
+// var names=["mano", "Paul", "Pierre"];
+
+
+// console.log (nom);
+
+// console.log ("salut" + nom);
+
+// console.log ("salut" + names[0]);
+
+// nom="Marc";
+
+// console.log ("salut" + nom);
+
+// const lastname="Durand";
+
+// lastName "muche"; Erreur
+
+let age=30;
+
+console.log (age)
+
+if (age>30) 
+{console.log("Vous pouvez conduire");}
+
+else if (age==30)
+{console.log("Vous avez 30 ans");}
+
+
+else (age<30)
+{console.log("Vous devez attendre ");}
+
+
+
+
