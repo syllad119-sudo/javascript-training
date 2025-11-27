aa6a5e762fa38c490b45779dc4a5141284096dfe
David <syllad119@gmail.com>
2025-11-27 10:06:12 +0100

    traiteexo7

---
+++ exo7.js
@@ -0,0 +1,22 @@
+let codeSecret = "Delta"; // Testez avec différentes valeurs
+
+switch (codeSecret) {
+    case "Alpha":
+        console.log("Lancer la promotion de la semaine!");
+        break;
+
+    case "Bravo":
+        console.log("Réorganiser la section des jeux rétro!");
+        break;
+
+    case "Charlie":
+        console.log("Préparer la zone de démonstration pour les nouveaux arrivages!");
+        break;
+
+    case "Delta":
+        console.log("Vérifier l'inventaire des accessoires!");
+}
+        
+
+
+   
